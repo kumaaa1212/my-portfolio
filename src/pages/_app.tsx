39a8e97935f6aba827822928778e818f645a5e23
Ryohei <harrry.kuma1225@gmail.com>
2024-02-13 20:06:25 +0900

    add react-query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,15 @@ import type { AppProps } from 'next/app'
 import '../styles/global/index.scss'
 import '../styles/reset/index.scss'
 import { ChakraProvider } from '@chakra-ui/react'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from '@/lib/react-query'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </QueryClientProvider>
   )
 }
